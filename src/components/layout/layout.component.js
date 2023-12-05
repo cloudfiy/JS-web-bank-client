80873e767624f8679c2ed484eadacb04512c93ac
Newbie <reynal@mail.ru>
2023-12-06 00:03:51 +0300

    Layout
    Добавление layout для рендера компонентов

---
+++ src/components/layout/layout.component.js
@@ -0,0 +1,17 @@
+export class Layout {
+	constructor({ router, children }) {
+		this.router = router
+		this.children = children
+	}
+
+	render() {
+		const headerHTML = '<header>Header</header>'
+
+		return `
+        ${headerHTML}
+        <main>
+        ${this.children}
+        </main>
+        `
+	}
+}
